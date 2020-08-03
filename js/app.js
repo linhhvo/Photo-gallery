e9785d109982f46cf910929f076049872f7a556b
Linh <lhvo2112@gmail.com>
2020-08-03 00:02:16 -0400

    Fix image captions

--- js/app.js
+++ js/app.js
@@ -4,9 +4,9 @@ let img_html = '';
 let searchField = document.getElementById('search-bar');
 let picList = document.getElementsByTagName('a');
 
-// Create map for picture titles
+// Create map for images' captions and alt-texts
 
-let picTitle = new Map([
+let imgAlt = new Map([
   [1, 'Hay Bales'],
   [2, 'Lake'],
   [3, 'Canyon'],
@@ -21,12 +21,51 @@ let picTitle = new Map([
   [12, 'Bluebells'],
 ]);
 
+let picCaption = new Map([
+  [
+    1,
+    'I love hay bales. Took this snap on a drive through the countryside past some straw fields.',
+  ],
+  [
+    2,
+    'The lake was so calm today. We had a great view of the snow on the mountains from here.',
+  ],
+  [
+    3,
+    'I hiked to the top of the mountain and got this picture of the canyon and trees below.',
+  ],
+  [
+    4,
+    'It was amazing to see an iceberg up close, it was so cold but didn’t snow today.',
+  ],
+  [
+    5,
+    'The red cliffs were beautiful. It was really hot in the desert but we did a lot of walking through the canyons.',
+  ],
+  [
+    6,
+    'Fall is coming, I love when the leaves on the trees start to change color.',
+  ],
+  [7, 'I drove past this plantation yesterday, everything is so green!'],
+  [8, 'My summer vacation to the Oregon Coast. I love the sandy dunes!'],
+  [9, 'We enjoyed a quiet stroll down this countryside lane. '],
+  [10, 'Sunset at the coast! The sky turned a lovely shade of orange.'],
+  [
+    11,
+    'I did a tour of a cave today and the view of the landscape below was breathtaking.',
+  ],
+  [
+    12,
+    'I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in.',
+  ],
+]);
+
 // Add anchor and image tags to HTML
 
-for (let i of picTitle) {
+for (let i = 1; i <= picCaption.size; i++) {
   img_html += `
-        <a href="photos/${i[0]}.jpg" title="${i[1]}">
-          <img src="photos/thumbnails/${i[0]}.jpg" alt="${i[1]}" />
+        <a href="photos/${i}.jpg" data-caption="${picCaption.get(i)}">
+          <img src="photos/thumbnails/${i}.jpg" alt="${imgAlt.get(i)}" />
         </a>`;
 }
 
@@ -35,9 +74,9 @@ document.querySelector('div').innerHTML = img_html;
 // Search filter function
 
 function searchFilter() {
-  for (let i = 0; i < picTitle.size; i++) {
+  for (let i = 0; i < picCaption.size; i++) {
     let searchInput = searchField.value.toLowerCase();
-    let caption = picTitle.get(i + 1).toLowerCase();
+    let caption = picCaption.get(i + 1).toLowerCase();
 
     if (caption.includes(searchInput)) {
       picList[i].style.display = ``;
